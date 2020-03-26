8958a49ee548d73cfbaacb4dbd6cac5f19b79dfe
our ame <you@example.com>
2020-03-26 15:49:33 +0700

    Добавил данные Продукт

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->sentence,
+        'anons' => $faker->paragraph,
+        'desc' => $faker->realText(),
+        'price' => $faker->numberBetween(100, 500),
+        'image' => 'https://picsum.photos/200/300',
     ];
 });
--- database/migrations/2020_03_26_083224_create_products_table.php
+++ database/migrations/2020_03_26_083224_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('anons');
+            $table->longText('desc');
+            $table->integer('price');
+            $table->string('image');
             $table->timestamps();
         });
     }
