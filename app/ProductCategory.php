4da4be28ab69fa8921f7b268bc01310053ea0cc1
our ame <you@example.com>
2020-03-29 21:10:24 +0700

    Добавил указание, какие поля обновлять

--- app/ProductCategory.php
+++ app/ProductCategory.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductCategory extends Model
 {
+    protected $fillable = ['title', 'anons', 'desc'];
+
     public function products()
     {
         return $this->hasMany('App\Product');
