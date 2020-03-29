43f6d6e26f7723a6c5ff75b2e796605accf66c9e
our ame <you@example.com>
2020-03-29 21:10:58 +0700

    Управление доступом к сохранению информации

--- app/Http/Middleware/myResponse.php
+++ app/Http/Middleware/myResponse.php
@@ -17,6 +17,7 @@ class myResponse
     {
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+        header ( "Access-Control-Allow-Headers: *") ;
 
         return $next($request);
     }
