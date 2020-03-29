e153092e746b9d7d777218726972115eda39a036
our ame <you@example.com>
2020-03-29 21:53:59 +0700

    fix csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'products/categories',
+        'products/categories/*',
         'products',
         'tasks',
     ];
