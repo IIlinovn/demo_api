9ec68ea1531e1e378c1544fe1f1aea9df3618b43
our ame <you@example.com>
2020-03-31 08:34:45 +0700

    добавил авторизацию. Перенес в марушрут получение списка продуктов

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,30 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'products'
+], function ($router) {
+    Route::get('', function () {
+        return new \App\Http\Resources\Product(\App\Product::all());
+    });
+    Route::get('{id}', function ($id) {
+        return new \App\Http\Resources\ProductResource(\App\Product::find($id));
+    });
+    Route::post('', 'ProductController@create');
+    Route::put('{id}', 'ProductController@update');
+});
